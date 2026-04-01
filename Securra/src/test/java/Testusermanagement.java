24c904b999a6b224d9ae8ede8a7e8c6dfe7caa6b
Santhosh <Santhosh@Laptop516>
2026-04-01 15:28:32 +0530

    Pipeline Testing and commenting the unused lines

--- Securra/src/test/java/Testusermanagement.java
+++ Securra/src/test/java/Testusermanagement.java
@@ -61,17 +61,17 @@ public class Testusermanagement extends Browserhandling {
 		System.out.println("error message verified");
 		
 	}
-	@Test (priority=3)
-	public void verifyemptyerrormsg()
-	{
-		Usermanagement usermgmt=new Usermanagement(driver);
-		driver.navigate().refresh();
-		 usermgmt.adduserrerrors();
-	
-		String exceptedmsg="Required Field cannot be left blank";
-		Assert.assertEquals(actualmsg, exceptedmsg);
-		System.out.println("mandatory error message has been verified");
-	}
+//	@Test (priority=3)
+//	public void verifyemptyerrormsg()
+//	{
+//		Usermanagement usermgmt=new Usermanagement(driver);
+//		driver.navigate().refresh();
+//		 usermgmt.adduserrerrors();
+//	
+//		String exceptedmsg="Required Field cannot be left blank";
+//		Assert.assertEquals(actualmsg, exceptedmsg);
+//		System.out.println("mandatory error message has been verified");
+//	}
 
 	
 	
